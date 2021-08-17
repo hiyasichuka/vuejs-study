2bc2ad7c60826d62aea202e35378f071db9a44e6
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2021-08-17 17:41:13 +0900

    vue.js sample

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
 (function() {
     'use strict';
-    // two way data binding (to UI)  
-    var vm = new Vue({
-      el: '#app',
-      data: {
-        name: '名前'
-      }
-    });
+    console.log("hello");
+    console.log(150*1279);
+    console.log(150*129);
+  // 
+    let price = 150
+    console.log(price*1279);
+    console.log(price*129);
+    price = 180
+    console.log(price*1279);
+    console.log(price*129);
+
+    // // two way data binding (to UI)  
+    // var vm = new Vue({
+    //   el: '#app',
+    //   data: {
+    //     name: '名前'
+    //   }
+    // });
   })();
